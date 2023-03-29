a02271f9d944a6dc318145e701946fe0f37d5da3
Kapza94 <luka.kapetanovic@icloud.com>
2023-03-29 05:48:55 +0200

    Adding project

---
+++ script.js
@@ -0,0 +1,50 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+const generateTarget = () =>{
+ return Math.floor(Math.random() * 9);
+}
+//BELOW - need to use math.abs() to see who is further away from the number that is generated.  
+const compareGuesses = (userGuess, computerGuess, secretTarget) => {
+const absDistanceUser = Math.abs(secretTarget - userGuess);
+const absDistanceComputer = Math.abs(secretTarget - computerGuess);
+if (userGuess === computerGuess || userGuess === secretTarget){
+return true
+} else if (computerGuess === secretTarget){
+return false
+} else if (absDistanceComputer < absDistanceUser){
+ return false
+} else if (absDistanceUser < absDistanceComputer){
+  return true
+}
+}
+
+
+// const updateScore = (winner) => {
+//   if (compareGuesses === true){
+//    return humanScore++
+//   } else {
+//     return computerScore++
+//   }
+//   }
+const updateScore = (winner) => {
+  if (winner === 'human') {
+        humanScore += 1;
+    } else if (winner === 'computer') {
+        computerScore += 1;
+    }
+}
+
+  const advanceRound = () => {
+    return currentRoundNumber + 1
+  }
+
